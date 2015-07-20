2a70d396834454a1d09a98aa21b5a4974dbf9ea0
Skander <skander.kc@gmaill.com>
2015-07-20 18:59:51 -0400

    Add paypal plugin

---
+++ server/config/paypal_config.js
@@ -0,0 +1,6 @@
+Meteor.Paypal.config({
+    'host': 'api.sandbox.paypal.com',
+    'port': '',
+    'client_id': 'AVipoeiIMBTOsqvoza2DO-pjiQAPydIpvVe_JSSiO6en9WlLeJJbtEJYgTOtdRqgEKNWtJFcSDPOi8IL',
+    'client_secret': 'ELF50D008DHy-0TSn085Cpf9V9G6mcxOeeraDXMetz3Xi-tUdJIrEFgIKF-KQMaBM3GjWMMgYJi_uVuK'
+});
